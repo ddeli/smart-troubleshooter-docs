be19936088eae3666eb56867217ac962452d1106
Deniz Deli <d.deli@campus.tu-berlin.de>
2025-06-07 17:01:15 +0200

    Init project structure

---
+++ backend/src/main/java/com/me/smart_troubleshooting_docs/SmartTroubleshootingDocsApplication.java
@@ -0,0 +1,13 @@
+package com.me.smart_troubleshooting_docs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTroubleshootingDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTroubleshootingDocsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/me/smart_troubleshooting_docs/SmartTroubleshootingDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.smart_troubleshooting_docs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTroubleshootingDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
