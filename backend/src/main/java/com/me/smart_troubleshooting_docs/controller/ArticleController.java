18b5b212ef0a7a3465e62c5705966675880a8a99
Deniz Deli <d.deli@campus.tu-berlin.de>
2025-08-16 13:39:50 +0200

    Backend: Add getArticles function

--- backend/src/main/java/com/me/smart_troubleshooting_docs/controller/ArticleController.java
+++ backend/src/main/java/com/me/smart_troubleshooting_docs/controller/ArticleController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -31,5 +32,12 @@ public class ArticleController {
         response.put("id", article.getId());
 
         return ResponseEntity.ok(response);
+    }
+    
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("/articles")
+    public ResponseEntity<List<Article>> getArticles() {
+        List<Article> articles = articleRepository.findAll();
+        return ResponseEntity.ok(articles);
     }
 }
